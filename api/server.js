730d3e69eb635b35996b490b5c8909e5507885a3
mrwan2546 <unturnedmc1@gmail.com>
2025-01-08 22:46:53 +0700

    fix: Some fix to Movie

--- api/server.js
+++ api/server.js
@@ -1,47 +1,177 @@
+const hapi = require('@hapi/hapi');
+var users = require('./users.js');
+const env = require('./env.js');
+const Movies = require('./repository/movie');
+
 const express = require('express');
-const path = require('path');
-const app = express(),
-      bodyParser = require("body-parser");
-      port = 80;
+const app = express();
 
-// place holder for the data
-const users = [
-  {
-    firstName: "first1",
-    lastName: "last1",
-    email: "abc@gmail.com"
-  },
-  {
-    firstName: "first2",
-    lastName: "last2",
-    email: "abc@gmail.com"
-  },
-  {
-    firstName: "first3",
-    lastName: "last3",
-    email: "abc@gmail.com"
-  }
-];
+//------------------
+const api_port = 3001;
+const web_port = 3010;
 
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '../my-app/build')));
 
-app.get('/api/users', (req, res) => {
-  console.log('api/users called!')
-  res.json(users);
+//------------- express ----------
+app.get('/', (req, res) => {
+  res.send('<h1> Hello World main_server </h1>');
+
+})
+
+app.get('/users', function (req, res) {
+  res.json(users.findAll());
 });
 
-app.post('/api/user', (req, res) => {
-  const user = req.body.user;
-  console.log('Adding user:::::', user);
-  users.push(user);
-  res.json("user addedd");
+app.get('/user/:id', function (req, res) {
+  var id = req.params.id;
+  res.json(users.findById(id));
 });
 
-app.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname, '../my-app/build/index.html'));
+app.listen(web_port, () => {
+  console.log('Start web server at port ' + web_port);
+
+})
+
+//------------ hapi --------------
+
+console.log('Running Environment: ' + env);
+
+
+const init = async () => {
+
+  const server = hapi.Server({
+    port: api_port,
+    host: '0.0.0.0',
+    routes: {
+      cors: true
+    }
+  });
+
+  //---------
+
+  await server.register(require('@hapi/inert'));
+
+  server.route({
+    method: "GET",
+    path: "/",
+    handler: () => {
+      return '<h3> Welcome to API Back-end Ver. 1.0.0</h3>';
+    }
+  });
+
+
+    //API: http://localhost:3001/api/movie/all
+    server.route({
+      method: 'GET',
+      path: '/api/movie/all',
+      config: {
+          cors: {
+              origin: ['*'],
+              additionalHeaders: ['cache-control', 'x-requested-width']
+          }
+      },
+      handler: async function (request, reply) {
+          //var param = request.query;
+          //const category_code = param.category_code;
+
+          try {
+
+              const responsedata = await Movies.MovieRepo.getMovieList();
+              if (responsedata.error) {
+                  return responsedata.errMessage;
+              } else {
+                  return responsedata;
+              }
+          } catch (err) {
+              server.log(["error", "home"], err);
+              return err;
+          }
+          
+      }
+  });
+
+    server.route({
+      method: 'GET',
+      path: '/api/movie/search',
+      config: {
+          cors: {
+              origin: ['*'],
+              additionalHeaders: ['cache-control', 'x-requested-width']
+          }
+      },
+      handler: async function (request, reply) {
+          var param = request.query;
+          const search_text = param.search_text;
+          //const title = param.title;
+
+          try {
+
+            const responsedata = await Movies.MovieRepo.getMovieSearch(search_text);
+            if (responsedata.error) {
+                return responsedata.errMessage;
+            } else {
+                return responsedata;
+            }
+        } catch (err) {
+            server.log(["error", "home"], err);
+            return err;
+        }
+
+      }
+  });
+
+
+  server.route({
+    method: 'POST',
+    path: '/api/movie/insert',
+    config: {
+        payload: {
+            multipart: true,
+        },
+        cors: {
+            origin: ['*'],
+            additionalHeaders: ['cache-control', 'x-requested-width']
+        }
+    },
+    handler: async function (request, reply) {
+
+        const {
+          title,
+          genre,
+          director,
+          release_year
+        } = request.payload;
+
+        //const title = request.payload.title;
+        //const genre = request.payload.genre;
+
+        try {
+
+          const responsedata = await Movies.MovieRepo.postMovie(title, genre, director,release_year);
+          if (responsedata.error) {
+              return responsedata.errMessage;
+          } else {
+              return responsedata;
+          }
+      } catch (err) {
+          server.log(["error", "home"], err);
+          return err;
+      }
+
+    }
 });
 
-app.listen(port, () => {
-    console.log(`Server listening on the port::${port}`);
+
+  await server.start();
+  console.log('API Server running on %s', server.info.uri);
+
+  //---------
+};
+
+
+process.on('unhandledRejection', (err) => {
+
+  console.log(err);
+  process.exit(1);
 });
+
+init();
